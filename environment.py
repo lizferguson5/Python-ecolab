e0df2fcdc82c93c299fa497f8ccebec10d1466eb
Mike Croucher <michael.p.croucher@googlemail.com>
2014-07-29 08:51:41 +0100

    Documentation and pep8 clean up

--- environment.py
+++ environment.py
@@ -1,41 +1,62 @@
 import numpy as np
-from agents import rabbit,fox
+from agents import rabbit, fox
 from numpy.random import rand
 
+
 class environment:
+    """An environment contains the area in which the agents live along
+       with the agents themselves.
+
+       Various model parameters such as simulation mode ('sync or 'async')
+       are also stored here.
+    """
+
     def __init__(self, size, mode='sync'):
-        self.bm_size= size
-        
+        self.bm_size = size
+
         # distribute food - 50 per square
-        self.food= np.floor(50 * np.ones((size,size)))
+        self.food = np.floor(50 * np.ones((size, size)))
 
         # generate patch where there is no food
-        self.food[np.round(0.6 * size)-1:np.round(0.8*size),np.round(0.6*size)-1:np.round(0.8*size)] = 0
+        self.food[np.round(0.6 * size)-1:np.round(0.8*size),
+                  np.round(0.6*size)-1:np.round(0.8*size)] = 0
 
-        # Is this model syncronous or asyncronous?
+        # Is this model synchronous or asynchronous?
         self.mode = mode
 
-    def create_agents(self,nr, nf, list_type='joined'):
+    def create_agents(self, nr, nf, list_type='joined'):
         """
-        create_agents(nr, nf)
+        environment.create_agents(nr, nf) - Creates a list of agents
+        within the environment
 
-        Creates a list of agents within the environment
+        Input Parameters
+        ----------------
 
-        Input:
+        nr : integer
+            Number of rabbits
 
-        nr - Integer: Number of rabbits
-        nf - Integer: Number of foxes
-        list_type - String: 'join' or 'separate' 
-                    'joined' - maintains a single list containing both rabbits and foxes
+        nf : integer
+            Number of foxes
+
+        list_type : string (default='joined')
+            Determines how the agents are stored.
+                    'joined' - maintains a single list containing both
+                    rabbits and foxes.
+
                     'seprate' - maintains separate lists of rabbits and foxes
+                    (THIS WONT WORK WITH THE REST OF ECOLAB YET)
 
 
-        output:
-        agent_list - Lists of generated agents
+        Output:
+        -------
+
+        agent_list : list
+            List(s) of generated agents
 
 
         """
-        # We create the transpose here in order to generate matrices that are identical to MATLAB's
+        # We create the transpose here in order to generate matrices
+        # that are identical to MATLAB's
         # generate random initial positions for rabbits and foxes
         rloc = np.transpose((self.bm_size-1)*rand(2, nr))+1
         floc = np.transpose((self.bm_size-1)*rand(2, nf))+1
